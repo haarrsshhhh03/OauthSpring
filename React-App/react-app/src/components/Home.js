fb9047aa74feaffa52d96baa2737b6174d5d6764
haarrsshhhh03 <harshkadhane97@gmail.com>
2025-03-05 12:32:57 +0530

    ADDED GOOGLE LOGIN INFO and react js

--- React-App/react-app/src/components/Home.js
+++ React-App/react-app/src/components/Home.js
@@ -1,8 +1,20 @@
 import React from "react";
+import App from "../App";
 const Home =() =>{
 
+    const GOOGLELOGIN=()=>{
 
+    };
+    const GitHubLOGIN=()=>{
+
+    };
     return(
-        <div></div>
+        <div>
+            <h2>Welcome to Oauth Demo</h2>
+            <button onClick={GOOGLELOGIN}>Login with GOOGLE LOGIN</button>
+            <button onClick={GitHubLOGIN}>Login with Github LOGIN</button>
+
+        </div>
     )
-}
+};
+export default Home;
